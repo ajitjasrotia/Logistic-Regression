a3dcdb69cc1f24d404ca360edcc18974e3cc33b9
Ajit singh Jasrotia <ajitjasrotia012@gmail.com>
2018-01-31 22:01:34 +0530

    Update Logistic Regression.py

--- Logistic Regression.py
+++ Logistic Regression.py
@@ -1,40 +1,43 @@
 #Logistic Regression Algorithm
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-#Load Training and Testing Data Set
-train = data.data
-test = data.target
+np.random.seed(10)
+num_observations = 1000
 
-#Training Data for Logistic Regression
-X_train = train[]
-Y_train = train[]
+x1 = np.random.multivariate_normal([0, 0], [[1, .75],[.75, 1]], num_observations)
+x2 = np.random.multivariate_normal([1, 4], [[1, .75],[.75, 1]], num_observations)
+
+features = np.vstack((x1, x2)).astype(np.float32)
+labels = np.hstack((np.zeros(num_observations), np.ones(num_observations)))
+
+def sigmoid(scores):
+    return 1 / (1 + np.exp(-scores))
  
-
-#Testing Data for Logistic Regression 
-X_test = test[]
-Y_test = test[]
-
-
-#Parameters Setup
-rate = 0.001
-iterations = 1000
-weights = np.zeros((X_train.shape[1],1))
-gradient = np.zeros((X_train.shape[1],1))
-
-#Function to Calcualte Hypothesis
-def sigmoid(data):
-    return 1 / (1+np.exp(-data))
-
-#Function to Minimize theta
-def minimize(th,grad):
-    th = th - (rate * grad)
-    return th
-
-#Function for Likelihood 
-
-#Iterations for Gradient Descent
-for i in range(iterations):
-    predict = X_train.dot(theta)
-    prediction = sigmoid(predict)
+def log_likelihood(features, target, weights):
+    scores = np.dot(features, weights)
+    ll = np.sum( target*scores - np.log(1 + np.exp(scores)) )
+    return ll
+   
+def logistic_regression(features, target, num_steps, learning_rate, add_intercept = False):
+    if add_intercept:
+        intercept = np.ones((features.shape[0], 1))
+        features = np.hstack((intercept, features))
+        
+    weights = np.zeros(features.shape[1])
     
+    for step in range(num_steps):
+        scores = np.dot(features, weights)
+        predictions = sigmoid(scores)
+
+        # Update weights with gradient
+        output_error_signal = target - predictions
+        gradient = np.dot(features.T, output_error_signal)
+        weights += learning_rate * gradient
+        
+        # Print log-likelihood every so often
+        if step % 10000 == 0:
+            print(log_likelihood(features, target, weights))
+        
+    return weights   
